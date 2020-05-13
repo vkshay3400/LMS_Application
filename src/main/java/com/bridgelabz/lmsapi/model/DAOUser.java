4837525d21c3414edc847c641be90e8b340f6dea
vkshay3400 <akshayp2208@gmail.com>
2020-05-13 15:38:18 +0530

    Modified: Database table

--- src/main/java/com/bridgelabz/lmsapi/model/DAOUser.java
+++ src/main/java/com/bridgelabz/lmsapi/model/DAOUser.java
@@ -1,22 +1,23 @@
 package com.bridgelabz.lmsapi.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
-@Entity
-@Table
+@Table(name = "user")
+@Entity(name = "user")
 public class DAOUser {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    @Column
-    private String username;
-    @Column
-    @JsonIgnore
+    private String first_name;
+    private String last_name;
+    private String email;
     private String password;
+    private long contact_number;
+    private String verified;
+    private LocalDateTime creator_stamp;
+    private String creator_user;
 
     public long getId() {
         return id;
@@ -26,12 +27,28 @@ public class DAOUser {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
+    public String getFirst_name() {
+        return first_name;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
@@ -40,5 +57,37 @@ public class DAOUser {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public long getContact_number() {
+        return contact_number;
+    }
+
+    public void setContact_number(long contact_number) {
+        this.contact_number = contact_number;
+    }
+
+    public String getVerified() {
+        return verified;
+    }
+
+    public void setVerified(String verified) {
+        this.verified = verified;
+    }
+
+    public LocalDateTime getCreator_stamp() {
+        return creator_stamp;
+    }
+
+    public void setCreator_stamp(LocalDateTime creator_stamp) {
+        this.creator_stamp = creator_stamp;
+    }
+
+    public String getCreator_user() {
+        return creator_user;
+    }
+
+    public void setCreator_user(String creator_user) {
+        this.creator_user = creator_user;
     }
 }
