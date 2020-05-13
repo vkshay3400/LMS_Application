1f65e8a986dc977377de808b79b9f4e0454e0d56
vkshay3400 <akshayp2208@gmail.com>
2020-05-13 15:38:08 +0530

    Modified: Database table

--- src/main/java/com/bridgelabz/lmsapi/dto/UserDTO.java
+++ src/main/java/com/bridgelabz/lmsapi/dto/UserDTO.java
@@ -1,16 +1,48 @@
 package com.bridgelabz.lmsapi.dto;
 
+import java.time.LocalDateTime;
+
 public class UserDTO {
-
-    private String username;
+    private long id;
+    private String first_name;
+    private String last_name;
+    private String email;
     private String password;
+    private long contact_number;
+    private String verified;
+    private LocalDateTime creator_stamp;
+    private String creator_user;
 
-    public String getUsername() {
-        return username;
+    public long getId() {
+        return id;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
@@ -19,5 +51,37 @@ public class UserDTO {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public long getContact_number() {
+        return contact_number;
+    }
+
+    public void setContact_number(long contact_number) {
+        this.contact_number = contact_number;
+    }
+
+    public String getVerified() {
+        return verified;
+    }
+
+    public void setVerified(String verified) {
+        this.verified = verified;
+    }
+
+    public LocalDateTime getCreator_stamp() {
+        return creator_stamp;
+    }
+
+    public void setCreator_stamp(LocalDateTime creator_stamp) {
+        this.creator_stamp = creator_stamp;
+    }
+
+    public String getCreator_user() {
+        return creator_user;
+    }
+
+    public void setCreator_user(String creator_user) {
+        this.creator_user = creator_user;
     }
 }
