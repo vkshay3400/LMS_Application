5310a1f6db22b6cb04c47c20129b91de093dacbc
vkshay3400 <akshayp2208@gmail.com>
2020-05-15 18:52:22 +0530

    Modified: Authorization for forget password

--- src/main/java/com/bridgelabz/lmsapi/configuration/SecurityConfigure.java
+++ src/main/java/com/bridgelabz/lmsapi/configuration/SecurityConfigure.java
@@ -31,7 +31,8 @@ public class SecurityConfigure extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/authenticate", "/register" ).permitAll()
+                .authorizeRequests().antMatchers("/authenticate", "/register","/send-mail",
+                "/change-password" ).permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
