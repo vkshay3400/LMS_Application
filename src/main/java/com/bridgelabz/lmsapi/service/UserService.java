fe1e24b94c6de9deae5774e1fc147542e264a254
vkshay3400 <akshayp2208@gmail.com>
2020-05-18 23:15:41 +0530

    Refactor: Changes in class made interface

--- src/main/java/com/bridgelabz/lmsapi/service/UserService.java
+++ src/main/java/com/bridgelabz/lmsapi/service/UserService.java
@@ -3,47 +3,8 @@ package com.bridgelabz.lmsapi.service;
 import com.bridgelabz.lmsapi.dto.UserDTO;
 import com.bridgelabz.lmsapi.model.DAOUser;
 
-import com.bridgelabz.lmsapi.repository.UserRepository;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+public interface UserService {
+    DAOUser loadUserDetails(UserDTO userDTO);
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-
-@Service
-public class LoginUserDetailsService implements UserDetailsService {
-
-    @Autowired
-    public ModelMapper modelMapper;
-
-    @Autowired
-    UserRepository userRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        DAOUser user = userRepository.findByFirst_name(userName);
-
-        if (user == null) {
-            throw new UsernameNotFoundException("User with username: " + userName + " not found ");
-        }
-        return new org.springframework.security.core.userdetails.User(user.getFirst_name(), user.getPassword(),
-                new ArrayList<>());
-    }
-
-    public DAOUser loadUserDetails(UserDTO userDTO) {
-        DAOUser daoUser = (DAOUser) modelMapper.map(userDTO, DAOUser.class);
-        daoUser.setCreator_stamp(LocalDateTime.now());
-        userRepository.save(daoUser);
-        return daoUser;
-    }
-
-    public DAOUser changePassword(UserDTO userDTO) {
-        DAOUser daoUser = userRepository.findByEmail(userDTO.getEmail());
-        daoUser.setPassword(userDTO.getPassword());
-        return userRepository.save(daoUser);
-    }
+    DAOUser changePassword(UserDTO userDTO);
 }
