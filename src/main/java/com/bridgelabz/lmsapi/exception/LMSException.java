7215e0eb21ee141b4da49d620b792537019dc0ce
vkshay3400 <akshayp2208@gmail.com>
2020-05-25 11:16:27 +0530

    Added: Exceptions

---
+++ src/main/java/com/bridgelabz/lmsapi/exception/LMSException.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.lmsapi.exception;
+
+public class LMSException extends RuntimeException{
+    private static final long serialVersionUID = 5776681206288518465L;
+
+    private exceptionType type;
+    private String message;
+
+    public LMSException(exceptionType type, String message) {
+        this.type = type;
+        this.message=message;
+    }
+
+    public enum exceptionType {
+        USER_NOT_FOUND, INVALID_PASSWORD,INVALID_ID, INVALID_EMAIL_ID, INVALID_TOKEN, DATA_NOT_FOUND;
+    }
+}
