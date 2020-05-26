23dc7c8ffe79b221ccd73b7b2cbce3b6d986ee6d
vkshay3400 <akshayp2208@gmail.com>
2020-05-26 20:14:23 +0530

    Added: Fellowship service

---
+++ src/main/java/com/bridgelabz/lmsapi/service/FellowshipCandidateService.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.lmsapi.service;
+
+public interface FellowshipCandidateService {
+    void getUpdateDetails();
+
+    int getFellowshipCount();
+}
--- src/main/java/com/bridgelabz/lmsapi/service/FellowshipCandidateServiceImpl.java
+++ src/main/java/com/bridgelabz/lmsapi/service/FellowshipCandidateServiceImpl.java
@@ -1,4 +1,45 @@
 package com.bridgelabz.lmsapi.service;
 
-public class FellowshipCandidateServiceImpl {
+import com.bridgelabz.lmsapi.model.CandidateDao;
+import com.bridgelabz.lmsapi.model.FellowshipDao;
+import com.bridgelabz.lmsapi.repository.FellowshipCandidateRepository;
+import com.bridgelabz.lmsapi.repository.HiredCandidateRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FellowshipCandidateServiceImpl implements FellowshipCandidateService {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private HiredCandidateRepository hiredCandidateRepository;
+
+    @Autowired
+    private FellowshipCandidateRepository fellowshipCandidateRepository;
+
+    // Method to save data in fellowship database table
+    @Override
+    public void getUpdateDetails() {
+        List<CandidateDao> list = hiredCandidateRepository.findAll();
+        for (CandidateDao candidate : list) {
+            if (candidate.getStatus().matches("Accept")) {
+                FellowshipDao fellowshipDao = modelMapper
+                        .map(candidate, FellowshipDao.class);
+                fellowshipCandidateRepository.save(fellowshipDao);
+            }
+        }
+    }
+
+    // Method to get count from fellowship database table
+    @Override
+    public int getFellowshipCount() {
+        List list = fellowshipCandidateRepository.findAll();
+        return list.size();
+
+    }
 }
