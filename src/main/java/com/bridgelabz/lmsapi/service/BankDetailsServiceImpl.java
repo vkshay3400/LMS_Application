e181a37a813775a3d1d4fa3a1e79610abfabc571
vkshay3400 <akshayp2208@gmail.com>
2020-05-27 19:44:04 +0530

    Added: Bank details

---
+++ src/main/java/com/bridgelabz/lmsapi/service/BankDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.lmsapi.service;
+
+import com.bridgelabz.lmsapi.dto.BankDetailsDto;
+import com.bridgelabz.lmsapi.model.BankDetailsDao;
+import com.bridgelabz.lmsapi.repository.BankDetailsRepository;
+import com.bridgelabz.lmsapi.repository.FellowshipCandidateRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class BankDetailsServiceImpl implements BankDetailsService {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private BankDetailsRepository bankDetailsRepository;
+
+    @Autowired
+    private FellowshipCandidateRepository fellowshipCandidateRepository;
+
+    // Method to update bank details
+    @Override
+    public void saveBankDetails(BankDetailsDto bankDetailsDto) {
+        fellowshipCandidateRepository.findById(bankDetailsDto.getCandidateId());
+        BankDetailsDao bankDetailsDao = modelMapper
+                .map(bankDetailsDto, BankDetailsDao.class);
+        bankDetailsDao.setCreatorStamp(LocalDateTime.now());
+        bankDetailsRepository.save(bankDetailsDao);
+    }
+}
