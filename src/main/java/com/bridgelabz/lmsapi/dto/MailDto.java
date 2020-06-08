f5c74e64b194a89e0df7775e10141a4e574d6e22
vkshay3400 <akshayp2208@gmail.com>
2020-06-08 13:19:06 +0530

    Added: Dto for mail

--- src/main/java/com/bridgelabz/lmsapi/dto/MailDto.java
+++ src/main/java/com/bridgelabz/lmsapi/dto/MailDto.java
@@ -7,10 +7,9 @@ import java.io.Serializable;
 
 @Component
 @Data
-public class RabbitMqDto implements Serializable {
+public class MailDto implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
     private String to;
     private String from;
     private String subject;
