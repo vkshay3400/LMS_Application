287a2ff33e44b4a9d76c7758514a584798be45da
vkshay3400 <akshayp2208@gmail.com>
2020-06-08 13:19:53 +0530

    Added: RabbitMq configuration

--- src/main/java/com/bridgelabz/lmsapi/configuration/RabbitMqConfig.java
+++ src/main/java/com/bridgelabz/lmsapi/configuration/RabbitMqConfig.java
@@ -1,4 +1,38 @@
 package com.bridgelabz.lmsapi.configuration;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RabbitMqConfig {
+
+    @Value("${spring.rabbitmq.template.exchange}")
+    private String exchangeName;
+
+    @Value("${spring.rabbitmq.template.default-receive-queue}")
+    private String queueName;
+
+    @Value("${spring.rabbitmq.template.routing-key}")
+    private String routingKey;
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName);
+    }
+
+    @Bean
+    DirectExchange exchange() {
+        return new DirectExchange(exchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+
 }
