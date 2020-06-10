bd7ce2e9e3aee55a9c9c427144cd25d15f29bbd3
vkshay3400 <akshayp2208@gmail.com>
2020-06-10 12:34:03 +0530

    Modified: Method for redis check

--- src/main/java/com/bridgelabz/lmsapi/filters/JwtRequestFilter.java
+++ src/main/java/com/bridgelabz/lmsapi/filters/JwtRequestFilter.java
@@ -1,8 +1,13 @@
 package com.bridgelabz.lmsapi.filters;
 
+import com.bridgelabz.lmsapi.exception.LMSException;
+import com.bridgelabz.lmsapi.model.UserDao;
 import com.bridgelabz.lmsapi.service.UserServiceImpl;
 import com.bridgelabz.lmsapi.util.JwtUtil;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,12 +23,19 @@ import java.io.IOException;
 
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
+//    private final String key = "Key";
+
+    @Value("redis.key")
+    private String keys;
 
     @Autowired
     private UserServiceImpl userDetailsService;
 
     @Autowired
     private JwtUtil jwtUtil;
+
+    @Autowired
+    private RedisTemplate<String, UserDao> redisTemplate;
 
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
@@ -32,14 +44,25 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         String jwt = null;
         String username = null;
 
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7);
-            username = jwtUtil.extractUserName(jwt);
+            try {
+                username = jwtUtil.extractUserName(jwt);
+                Object token = redisTemplate.opsForHash().get(keys, username);
+                if (jwt.equals(token))
+                    throw new LMSException(LMSException.exceptionType.INVALID_TOKEN, "Invalid Token");
+            } catch (IllegalArgumentException e) {
+                System.out.println("Unable to get JWT Token");
+            } catch (ExpiredJwtException e) {
+                System.out.println("JWT Token has expired");
+            }
+        } else {
+            logger.warn("JWT Token does not begin with Bearer String");
         }
 
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
-            if (jwtUtil.validateToken(jwt, userDetails)){
+            if (jwtUtil.validateToken(jwt, userDetails)) {
                 UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
                         userDetails, null, userDetails.getAuthorities());
                 usernamePasswordAuthenticationToken
