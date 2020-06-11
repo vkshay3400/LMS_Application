6516c583b01b1c15bffd0538fca5494a65f92275
vkshay3400 <akshayp2208@gmail.com>
2020-06-11 11:58:31 +0530

    Added: Configuration of AOP

--- src/main/java/com/bridgelabz/lmsapi/configuration/AopConfig.java
+++ src/main/java/com/bridgelabz/lmsapi/configuration/AopConfig.java
@@ -1,4 +1,52 @@
 package com.bridgelabz.lmsapi.configuration;
 
+import org.apache.commons.lang3.time.StopWatch;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
 public class AopConfig {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Before(value = "execution(* com.bridgelabz.lmsapi.service.*.*(..))")
+    public void before(JoinPoint joinPoint) {
+        logger.info("Before method ");
+        logger.info("Allowed execution for {}", joinPoint);
+    }
+
+    @Around("execution(* com.bridgelabz.lmsapi.service.*.*(..))")
+    public Object profileAllMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+
+        //Get intercepted method details
+        String className = methodSignature.getDeclaringType().getSimpleName();
+        String methodName = methodSignature.getName();
+        final StopWatch stopWatch = new StopWatch();
+        //Measure method execution time
+        stopWatch.start();
+        Object result = proceedingJoinPoint.proceed();
+        stopWatch.stop();
+
+        //Log method execution details
+        logger.info("Execution details " + className + "." + methodName + " " + ":: " + result + stopWatch.getTime());
+        return result;
+    }
+
+    @After(value = "execution(* com.bridgelabz.lmsapi.service.*.*(..))")
+    public void after(JoinPoint joinPoint) {
+        logger.info("After method ");
+        logger.info("After execution of {}", joinPoint);
+    }
+
+    @AfterReturning(value = "execution(* com.bridgelabz.lmsapi.service.*.*(..))", returning = "result")
+    public void afterReturning(JoinPoint joinPoint, Object result) {
+        logger.info("{} returned with value {}", joinPoint, result);
+    }
 }
