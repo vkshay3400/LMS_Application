95c9c99261a9b74d37b8020c78a56de1fc280c9c
vkshay3400 <akshayp2208@gmail.com>
2020-06-11 22:34:04 +0530

    Added: Annotation

--- src/main/java/com/bridgelabz/lmsapi/dto/MailDto.java
+++ src/main/java/com/bridgelabz/lmsapi/dto/MailDto.java
@@ -3,15 +3,20 @@ package com.bridgelabz.lmsapi.dto;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Component
 @Data
 public class MailDto implements Serializable {
 
+    @NotNull
     private String to;
+    @NotNull
     private String from;
+    @NotNull
     private String subject;
+    @NotNull
     private String body;
 
 }
