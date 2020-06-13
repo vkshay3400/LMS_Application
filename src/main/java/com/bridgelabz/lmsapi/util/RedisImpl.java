ebe43acf14ec0ae4672c65ef5ee1efad2cc76a7f
vkshay3400 <akshayp2208@gmail.com>
2020-06-13 12:16:26 +0530

    Refactor: Redis implementation

--- src/main/java/com/bridgelabz/lmsapi/util/RedisImpl.java
+++ src/main/java/com/bridgelabz/lmsapi/util/RedisImpl.java
@@ -7,17 +7,19 @@ import org.springframework.stereotype.Component;
 import java.io.Serializable;
 
 @Component
-public class RedisUtil implements Serializable {
+public class RedisImpl implements Serializable, Redis {
 
     private static final long serialVersionUID = 1L;
 
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
 
+    @Override
     public void save(String RedisKey, String email, String authenticationToken) {
         redisTemplate.opsForHash().put(RedisKey, email, authenticationToken);
     }
 
+    @Override
     public Object getRedisToken(String redisKey, String userName) {
         return redisTemplate.opsForHash().get(redisKey, userName);
     }
