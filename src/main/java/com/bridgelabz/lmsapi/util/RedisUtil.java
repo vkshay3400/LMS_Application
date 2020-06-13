bdbb7398c11837455be959004babbd46f779f692
vkshay3400 <akshayp2208@gmail.com>
2020-06-13 10:55:17 +0530

    Added: Util for redis

--- src/main/java/com/bridgelabz/lmsapi/util/RedisUtil.java
+++ src/main/java/com/bridgelabz/lmsapi/util/RedisUtil.java
@@ -1,4 +1,20 @@
 package com.bridgelabz.lmsapi.util;
 
-public class RedisUtil {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+@Component
+public class RedisUtil implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    public void save(String RedisKey, String email, String authenticationToken) {
+        redisTemplate.opsForHash().put(RedisKey, email, authenticationToken);
+    }
 }
